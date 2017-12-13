b559cad3484d5be31946c14977a9c15f109536aa
zyx <1272861475@qq.com>
2017-12-13 19:07:17 +0800

    '2.0'

--- yueji.sql
+++ yueji.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-12-13 11:55:32
+-- Generation Time: 2017-12-13 12:06:27
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,18 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `yueji`
 --
-
--- --------------------------------------------------------
-
---
--- 表的结构 `banners`
---
-
-CREATE TABLE IF NOT EXISTS `banners` (
-  `bid` int(10) NOT NULL AUTO_INCREMENT,
-  `bthumb` int(255) NOT NULL,
-  PRIMARY KEY (`bid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
